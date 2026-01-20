e3db341c5cc271298228edb331f8a3b9280339eb
Kabir Karim <kabir.karim.kkd@gmail.com>
2026-01-20 15:57:53 +0500

    Add Comments

--- File Handling/Palindrome.c
+++ File Handling/Palindrome.c
@@ -10,19 +10,20 @@ int main() {
         return 0;
     }   
     fscanf(fp, "%s", word); // Read word from file
-    fclose(fp);
+    fclose(fp); // closing the file
    
     for(int i = 0; word[i] != '\0'; i++) { //For case Sensative
+        //converting all Alphabet to upper case
         if(word[i] >= 'a' && word[i] <= 'z')
-            word[i] = word[i] - 32;
+            word[i] = word[i] - 32; 
     }
     len=strlen(word);
-    for (i = 0; word[i] != '\0'; i++) {
-        if (word[i] != word[len - i - 1]) {     // For Cheacking Palindrome 
-            printf("'%s' is NOT a Palindrome\n", word);
-            return 0;
+    for (i = 0; word[i] != '\0'; i++) {     // For Cheacking Palindrome 
+        if (word[i] != word[len - i - 1]) { //if the word doesn't match the its reverse 
+            printf("'%s' is NOT a Palindrome\n", word); // Then word will is not a pelindrome
+            return 0; // end the program
         }
     }
-    printf("'%s' is a Palindrome\n", word);
+    printf("'%s' is a Palindrome\n", word); // If the loop is completed and no if condition is applied Then Word Will be palindrome
     return 0;
 }
