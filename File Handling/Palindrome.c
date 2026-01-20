f7fb12324580865f827742a00e161c8eb8462af2
Kabir Karim <kabir.karim.kkd@gmail.com>
2026-01-20 15:43:46 +0500

    Case-Sensitive And Shorten The Code

--- File Handling/Palindrome.c
+++ File Handling/Palindrome.c
@@ -2,42 +2,27 @@
 #include <string.h>
 int main() {
     FILE *fp;
-    char word[100], rev[100];
-    int i, len, flag = 1;
-
-    // Open file
-    fp = fopen("data.txt", "r");
-
-    if (fp == NULL) {
+    char word[100];
+    int i, len;    
+    fp = fopen("data.txt", "r"); // Open file
+    if (fp == NULL) {   // Checking that file found or not
         printf("File not found!\n");
         return 0;
-    }
-
-    // Read word from file
-    fscanf(fp, "%s", word);
+    }   
+    fscanf(fp, "%s", word); // Read word from file
     fclose(fp);
-
-    len = strlen(word);
-
-    // Reverse the word
-    for (i = 0; i < len; i++) {
-        rev[i] = word[len - i - 1];
+   
+    for(int i = 0; word[i] != '\0'; i++) { 
+        if(word[i] >= 'a' && word[i] <= 'z')
+            word[i] = word[i] - 32;
     }
-    rev[i] = '\0';
-
-    // Compare
-    for (i = 0; i < len; i++) {
-        if (word[i] != rev[i]) {
-            flag = 0;
-            break;
+    len=strlen(word);
+    for (i = 0; word[i] != '\0'; i++) {
+        if (word[i] != word[len - i - 1]) {
+            printf("'%s' is NOT a Palindrome\n", word);
+            return 0;
         }
     }
-
-    // Output
-    if (flag == 1)
-        printf("'%s' is a Palindrome\n", word);
-    else
-        printf("'%s' is NOT a Palindrome\n", word);
-
+    printf("'%s' is a Palindrome\n", word);
     return 0;
 }
